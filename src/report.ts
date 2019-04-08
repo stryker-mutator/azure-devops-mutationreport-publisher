92dfc69b59f39d7f78bbcdc526b2e230cd23cc8b
Rouke Broersma <rouke.broersma@infosupport.com>
2019-04-08 21:48:48 +0200

    Use commonjs instead of amd even though tutorials said to use amd but then you get defined is undefined errors soooo...

--- src/report.ts
+++ src/report.ts
@@ -1,8 +1,9 @@
 import Controls = require("VSS/Controls");
+import DT_Client = require("TFS/Build/RestClient");
 import TFS_Build_Contracts = require("TFS/Build/Contracts");
 import TFS_Build_Extension_Contracts = require("TFS/Build/ExtensionContracts");
 
-export class InfoTab extends Controls.BaseControl {	
+export class ReportTab extends Controls.BaseControl {	
 	constructor() {
 		super();
 	}
@@ -16,24 +17,12 @@ export class InfoTab extends Controls.BaseControl {
 			// register your extension with host through callback
 			sharedConfig.onBuildChanged((build: TFS_Build_Contracts.Build) => {
 				this._initBuildInfo(build);	
-				
-				/*
-				* If any task uploaded some data using ##vso[task.addattachment] (https://github.com/Microsoft/vso-agent-tasks/blob/master/docs/authoring/commands.md)
-				* Then you could consume the data using taskclient
-				* sample code -
-				*/
-					// var taskClient = DT_Client.getClient();
-					// taskClient.getPlanAttachments(vsoContext.project.id, "build", build.orchestrationPlan.planId, "ATTACHMENT_TYPE_HERE").then((taskAttachments)=> {
-					// 	$.each(taskAttachments, (index, taskAttachment) => {
-					// 		if (taskAttachment._links && taskAttachment._links.self && taskAttachment._links.self.href) {
-					// 			var link = taskAttachment._links.self.href;
-					// 			var attachmentName = taskAttachment.name;
-					// 			// do some thing here
-					//			// see how to get auth https://www.visualstudio.com/en-us/docs/report/analytics/building-extension-against-analytics-service
-					// 		}
-					// 	});
-					// });
-				
+                
+                var taskClient = DT_Client.getClient();
+                taskClient.getArtifact(build.id, "Mutation Report", vsoContext.project.name).then((artifact: TFS_Build_Contracts.BuildArtifact) => {
+                    var $reportDiv = $('#html-report');
+                    $reportDiv.load("<iframe src=\"" + artifact.resource.url + "\"></iframe>");
+                });
 			});
 		}		
 	}
@@ -43,7 +32,7 @@ export class InfoTab extends Controls.BaseControl {
 	}
 }
 
-InfoTab.enhance(InfoTab, $(".build-info"), {});
+ReportTab.enhance(ReportTab, $(".build-info"), {});
 
 // Notify the parent frame that the host has been loaded
 VSS.notifyLoadSucceeded();
