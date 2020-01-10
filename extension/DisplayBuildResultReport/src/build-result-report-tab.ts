e439c03a080937dc2f001386b785dd2f0746d3b6
Rouke Broersma <Rouke.Broersma@infosupport.com>
2020-01-10 23:08:10 +0100

    Build report tab react code

--- extension/DisplayBuildResultReport/src/build-result-report-tab.ts
+++ extension/DisplayBuildResultReport/src/build-result-report-tab.ts
@@ -1,80 +1,57 @@
-console.log('test');
-// /// <reference types="vss-web-extension-sdk" />
+import * as React from "react";
+import * as SDK from "azure-devops-extension-sdk";
 
-// import Controls = require("VSS/Controls");
-// import VSS_Service = require("VSS/Service");
-// import TFS_Build_Contracts = require("TFS/Build/Contracts");
-// import TFS_Build_Extension_Contracts = require("TFS/Build/ExtensionContracts");
-// import DT_Client = require("TFS/DistributedTask/TaskRestClient");
+import { CommonServiceIds, IProjectPageService, IVssRestClientOptions } from "azure-devops-extension-api";
 
-// export class ReportTab extends Controls.BaseControl {	
-// 	constructor() {
-// 		super();
-// 	}
-		
-// 	public initialize(): void {
-// 		super.initialize();
+import { BuildServiceIds, IBuildPageDataService } from "azure-devops-extension-api/Build/BuildServices"
+import { BuildRestClient } from "azure-devops-extension-api/Build/BuildClient"
 
-// 		// Get configuration that's shared between extension and the extension host
-// 		var sharedConfig: TFS_Build_Extension_Contracts.IBuildResultsViewExtensionConfig = VSS.getConfiguration();
-// 		var vsoContext = VSS.getWebContext();
+class BuildResultTab extends React.Component<{}> {
 
-// 		if(sharedConfig) {
-// 			// register your extension with host through callback
-// 			sharedConfig.onBuildChanged((build: TFS_Build_Contracts.Build) => {
-// 				this._initBuildInfo(build);	
-                
-//                 var taskClient = DT_Client.getClient();
-//                 taskClient.getPlanAttachments(vsoContext.project.id, "build", build.orchestrationPlan.planId, "stryker-mutator.mutation-report").then((taskAttachments) => {
-// 				if (taskAttachments.length == 0) {
-// 					var element = $("<div />");
-		
-// 					var errorText = "No HTML report found!";
-		
-// 					element.html("<p>" + errorText + "</p>");
-// 					this._element.replaceWith(element);
-// 				} else {
-// 					$.each(taskAttachments, (_, taskAttachment) => {
-// 						if (taskAttachment._links && taskAttachment._links.self && taskAttachment._links.self.href) {
-			
-// 							taskClient.getAttachmentContent(vsoContext.project.id, "build", build.orchestrationPlan.planId, taskAttachment.timelineId, taskAttachment.recordId, "stryker-mutator.mutation-report", taskAttachment.name).then((attachmentContent) => {
-			
-// 								var text = new TextDecoder('utf-8').decode(new Uint8Array(attachmentContent));
-				
-// 								var reportFrame = $('<iframe>', {
-// 									srcdoc: text,
-// 									id: 'html-artifact',
-// 									frameborder: '0',
-// 									width: '100%',
-// 									height: '100%',
-// 									scrolling: 'yes',
-// 									marginheight: '0',
-// 									marginwidth: '0'
-// 									});
-				
-// 								this._element.replaceWith(reportFrame);
-				
-// 								VSS.resize();
-// 							})
-// 						}
-// 					});
-// 				}
-// 			}, function() {
-// 				var element = $("<div />");
-	
-// 				var errorText = "No HTML report found!";
-	
-// 				element.html("<p>" + errorText + "</p>");
-// 				this._element.replaceWith(element);
-// 				});
-// 			});
-// 		}		
-// 	}
-	
-// 	private _initBuildInfo(build: TFS_Build_Contracts.Build) {}
-// }
+    reportType : string = "stryker-mutator.mutation-report";
 
-// ReportTab.enhance(ReportTab, $("#html-report"), {});
+    public componentDidMount() {
+        SDK.init({ loaded: false });
+        this.initializeState();
+    }
 
-// // Notify the parent frame that the host has been loaded
-// VSS.notifyLoadSucceeded();
+    private async initializeState(): Promise<void> {
+        await SDK.ready();
+
+        const buildPageDataService = await SDK.getService<IBuildPageDataService>(BuildServiceIds.BuildPageDataService)
+        
+        const projectService = await SDK.getService<IProjectPageService>(CommonServiceIds.ProjectPageService);
+        const buildPage = buildPageDataService.getBuildPageData();
+        const project = await projectService.getProject();
+
+        if(buildPage?.build && project)
+        {
+            const buildClient = new BuildRestClient({});
+            const reportAttachments = await buildClient.getAttachments(project.id, buildPage.build.id, this.reportType);
+
+            if(reportAttachments.some(e => e))
+            {
+                const attachmentMetadata = reportAttachments[0];
+                const attachment = await buildClient.getAttachment(project.id, buildPage.build.id, "timelineId", "recordId", this.reportType, attachmentMetadata.name)
+
+                const attachmentText = new TextDecoder('utf-8').decode(new Uint8Array(attachment));
+
+                // Now we set component state with attachmentText
+
+                SDK.notifyLoadSucceeded().then(() => {
+                    // we are visible in this callback. Resize window to fit needs.
+                    SDK.resize();
+                });
+            }
+        }
+
+        SDK.notifyLoadFailed("No HTML report found..");
+    }
+
+    public render(): JSX.Element {
+
+        // here we render the iframe with attachmentText as source
+
+        return null;
+    }
+}
