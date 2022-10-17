d3833b956d47ec6f8816a18ab72f291238bc6a7f
Maxime Dandel <maxime.dandel@suez.com>
2022-10-17 15:35:33 +0200

    Allow to display the proper report's name

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Mutation Report Publisher
+npm# Mutation Report Publisher
 
 Have you been trying out mutation testing using [Stryker](https://stryker-mutator.io) or any of the other mutation testing frameworks? Have you noticed that you forget to check the mutation results? Then this extension is for you.
 
@@ -17,4 +17,4 @@ You can test changes to the web extension by running the `npm run start:dev` com
 You can compile the web contribution by running the `npm run compile:dev` command from the `extension/PublishMutationReport` directory.
 You can test changes to the azure pipelines task by running the `node main.js` command from the `extension/PublishMutationReport/dist` folder after compilation.
 
-For a successful run you need to set the environment variable `$Env:reportPattern` to a filepath
+For a successful run you need to set the environment variable `$Env:INPUT_reportPattern` to a filepath
--- extension/PublishMutationReport/src/publish.ts
+++ extension/PublishMutationReport/src/publish.ts
@@ -1,34 +1,43 @@
 import * as tl from 'azure-pipelines-task-lib';
+import * as path from 'path';
 
 function publish() {
     try {
-        var reportPaths = findReports();
+        const reportPattern: string = tl.getInput('reportPattern', true) as string;
+        var reportPaths = findReports(reportPattern);
         let reportName: string = tl.getInput('reportDisplayName', false) as string;
+        let useOriginalReportName: boolean = tl.getBoolInput('isReportWellNamed', false);
 
-        if (!reportName?.trim()) {
+
+
+        if (!reportName?.trim() && !useOriginalReportName) {
           reportName = 'mutation-report';
         }
 
         for (let i = 0; i < reportPaths.length; i++) {
             const element = reportPaths[i];
-
             let currentReport = i+1;
             let progress = currentReport / reportPaths.length * 100;
             tl.setProgress(progress, "Uploading reports");
             console.info("Uploading report " + currentReport + " of " + reportPaths.length);
+            if (useOriginalReportName){
+                reportName =  path.basename(element);
+                tl.addAttachment("stryker-mutator.mutation-report", reportName, element);
+                continue;
+            }
+
             tl.addAttachment("stryker-mutator.mutation-report", reportName+"-"+currentReport+".html", element);
         }
         
         tl.setResult(tl.TaskResult.Succeeded, "");
     }
     catch (err) {
+        // @ts-ignore
         tl.setResult(tl.TaskResult.Failed, err.message);
     }
 }
 
-function findReports(): string[] {
-    
-    const reportPattern: string = tl.getInput('reportPattern', true) as string;
+function findReports(reportPattern: string): string[] {
 
     if(reportPattern.length == 0) {
         tl.setResult(tl.TaskResult.Failed, "Report filepath cannot be empty. Please provide a path to the report.", true);
